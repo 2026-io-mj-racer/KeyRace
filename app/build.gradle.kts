14b0fe30c3cd4ce93f4cd5fb01ed8aa89c4b3f86
mjedrkowiak <mateuszjedrkowiak2@gmail.com>
2026-04-21 11:33:45 +0200

    add: app theme color set, remove redundant .xml items

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
 
+    implementation(libs.androidx.core.splashscreen)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
