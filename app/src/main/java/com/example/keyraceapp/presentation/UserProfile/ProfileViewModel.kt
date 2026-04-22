d908850fa801d39b2026c5b90b5645dbc3bec3eb
mjedrkowiak <mateuszjedrkowiak2@gmail.com>
2026-04-22 15:48:58 +0200

    refactor: hoist duplicated logic into fetchTypingData and fetchWordsAndGames :)

--- app/src/main/java/com/example/keyraceapp/presentation/UserProfile/ProfileViewModel.kt
+++ app/src/main/java/com/example/keyraceapp/presentation/UserProfile/ProfileViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.keyraceapp.domain.models.Difficulty
 import com.example.keyraceapp.domain.models.GameMode
+import com.example.keyraceapp.domain.models.Score
 import com.example.keyraceapp.domain.models.TimePeriod
 import com.example.keyraceapp.domain.repositories.ScoreRepository
 import com.example.keyraceapp.domain.repositories.UserRepository
@@ -14,8 +15,8 @@ import com.example.keyraceapp.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -28,13 +29,12 @@ class ProfileViewModel @Inject constructor(
         private set
     fun onEvent(event: ProfileEvent) {
         when(event) {
-            is ProfileEvent.OnFetchArcade -> viewModelScope.launch { fetchArcadeData() }
-            is ProfileEvent.OnFetchTraining -> viewModelScope.launch { fetchTrainingData() }
+            is ProfileEvent.OnFetchArcade -> viewModelScope.launch { fetchTypingData(GameMode.Arcade(Difficulty.EASY)) }
+            is ProfileEvent.OnFetchTraining -> viewModelScope.launch { fetchTypingData(GameMode.Training.TimeBased(TimePeriod.THIRTY_SECONDS))}
             is ProfileEvent.OnResetUserData -> viewModelScope.launch { resetUserData() }
             is ProfileEvent.OnFetchUser -> viewModelScope.launch { fetchUser() }
         }
     }
-
     private suspend fun fetchUser() {
         val user = userRepository.getUser()
 
@@ -43,11 +43,17 @@ class ProfileViewModel @Inject constructor(
             is Resource.Error, is Resource.Loading -> stateUpdaterLoadingOrError(user)
         }
     }
-    private suspend fun fetchArcadeData() {
 
+    private suspend fun fetchTypingData(mode: GameMode) {
         coroutineScope {
-            this.launch {
-                scoreRepository.getTopTenArcade()
+
+            launch {
+                val flow = when(mode) {
+                    is GameMode.Training ->  scoreRepository.getTopTenTraining() as Flow<Resource<List<Score>>>
+                    is GameMode.Arcade -> scoreRepository.getTopTenArcade() as Flow<Resource<List<Score>>>
+                }
+
+                flow
                     .catch {e -> state = state.copy(errorMessage = e.message) }
                     .collect { value ->
                         when(value) {
@@ -63,6 +69,11 @@ class ProfileViewModel @Inject constructor(
                     }
             }
 
+            fetchTotalWordsAndGames()
+        }
+    }
+    private suspend fun fetchTotalWordsAndGames() {
+        coroutineScope {
             val totalWordsPromise = async { scoreRepository.getTotalWords() }
             val totalGamesPromise = async { scoreRepository.getTotalGames() }
 
@@ -75,53 +86,16 @@ class ProfileViewModel @Inject constructor(
                 is Resource.Success -> state = state.copy(wordsTyped = totalWords.data!!)
                 is Resource.Error, is Resource.Loading -> stateUpdaterLoadingOrError(totalWords)
             }
-
-        }
-    }
-    private suspend fun fetchTrainingData() {
-        coroutineScope {
-            launch {
-                scoreRepository.getTopTenTraining()
-                    .catch {e -> state = state.copy(errorMessage = e.message) }
-                    .collect { value ->
-                        when(value) {
-                            is Resource.Success -> {
-                                state = state.copy(
-                                    topScores = value.data!!,
-                                    topWpm = value.data.firstOrNull()?.wpm ?: 0f,
-                                )
-                            }
-
-                            is Resource.Error, is Resource.Loading -> stateUpdaterLoadingOrError(value)
-                        }
-                    }
-            }
-
-            val wordsTypedDeferred = async { scoreRepository.getTotalWords() }
-            val gamesPlayedDeferred = async { scoreRepository.getTotalGames() }
-
-            when(val totalWords = wordsTypedDeferred.await()) {
-                is Resource.Success -> state = state.copy(wordsTyped = totalWords.data!!)
-                is Resource.Loading, is Resource.Error -> stateUpdaterLoadingOrError(totalWords)
-            }
-
-            when(val gamesPlayed = gamesPlayedDeferred.await()) {
-                is Resource.Success -> state = state.copy(gamesPlayed = gamesPlayed.data!!)
-                is Resource.Loading, is Resource.Error -> stateUpdaterLoadingOrError(gamesPlayed)
-            }
         }
     }
     private suspend fun resetUserData() {
         val response = userRepository.resetData(state.user)
         when(response) {
             is Resource.Success -> {
-                println("HERERERERERE")
                 state = ProfileState()
             }
             is Resource.Loading, is Resource.Error -> stateUpdaterLoadingOrError(response)
         }
-
-
     }
     private fun<T> stateUpdaterLoadingOrError(value: Resource<T>) {
         state = when(value) {
