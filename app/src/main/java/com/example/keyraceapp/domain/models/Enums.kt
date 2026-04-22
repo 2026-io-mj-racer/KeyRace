3308e56b8bc891e1bb1cd409709137e6d85212ae
mjedrkowiak <mateuszjedrkowiak2@gmail.com>
2026-04-22 06:41:33 +0200

    add: toString implementation for enums

--- app/src/main/java/com/example/keyraceapp/domain/models/Enums.kt
+++ app/src/main/java/com/example/keyraceapp/domain/models/Enums.kt
@@ -1,9 +1,12 @@
 package com.example.keyraceapp.domain.models
 
-enum class Difficulty {
-    EASY,
-    MEDIUM,
-    HARD;
+enum class Difficulty(val value: String = "EASY") {
+    EASY("EASY"),
+    MEDIUM("MEDIUM"),
+    HARD("HARD");
+
+
+    override fun toString() = this.value
 
     companion object {
         fun stringToDifficulty(s: String): Difficulty  =
@@ -16,19 +19,23 @@ enum class Difficulty {
     }
 }
 
-enum class TimePeriod {
-    FIFTEEN_SECONDS,
-    THIRTY_SECONDS,
-    FORTY_FIVE_SECONDS,
-    SIXTY_SECONDS
+enum class TimePeriod(val value: String) {
+
+    FIFTEEN_SECONDS("15"),
+    THIRTY_SECONDS("30"),
+    FORTY_FIVE_SECONDS("45"),
+    SIXTY_SECONDS("60");
+
+    override fun toString() = value
 }
 
-enum class WordCount {
-    TEN_WORDS,
-    TWENTY_WORDS,
-    THIRTY_WORDS,
-    FORTY_WORDS,
-    FIFTY_WORDS
+enum class WordCount(val value: String) {
+    TEN_WORDS("10"),
+    TWENTY_WORDS("20"),
+    THIRTY_WORDS("30"),
+    FORTY_WORDS("40"),
+    FIFTY_WORDS("50");
+    override fun toString() = value
 }
 
 enum class GameStatus {
