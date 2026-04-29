3bcac1001e05992a847329229a309ef85054b085
mjedrkowiak <mateuszjedrkowiak2@gmail.com>
2026-04-29 16:55:01 +0200

    Add cursor to TextInputLine

--- app/src/main/java/com/example/keyraceapp/presentation/Game/TextInputLine.kt
+++ app/src/main/java/com/example/keyraceapp/presentation/Game/TextInputLine.kt
@@ -1,13 +1,18 @@
 package com.example.keyraceapp.presentation.Game
 
+import android.R.attr.x
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.keyraceapp.ui.theme.DeepWhite
 import com.example.keyraceapp.ui.theme.ErrorRed
@@ -19,25 +24,33 @@ fun TextInputLine(targetText: String,typedText: String, startIndex: Int, lineTar
         horizontalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxWidth()
     ) {
-
         for((index, value) in lineTargetText.withIndex()) {
             val relativeIndex = startIndex + index
 
             val isRed =  typedText.length > relativeIndex && typedText[relativeIndex] != targetText[relativeIndex]
             val isGray = relativeIndex >= typedText.length
-
             val letterToDisplay =
-                if(typedText.length > relativeIndex) {
-                    typedText[relativeIndex]
+                if(typedText.length  > relativeIndex && typedText[relativeIndex] == targetText[relativeIndex]) {
+                    typedText[relativeIndex].toString()
                 } else {
-                    value
+                    value.toString()
                 }
 
-            Text(
-                text = letterToDisplay.toString(),
-                color = if(isGray) Color.Gray else if(isRed) ErrorRed else DeepWhite,
-                fontSize = 24.sp,
-            )
+            Box(modifier = Modifier.width(16.dp), contentAlignment = Alignment.Center){
+                if(relativeIndex == typedText.length) {
+                    Text(
+                        text = "|",
+                        color = DeepWhite,
+                        fontSize = 24.sp,
+                        modifier = Modifier.align(Alignment.CenterStart).offset(x = (-8).dp)
+                    )
+                }
+                Text(
+                    text = letterToDisplay,
+                    color = if(isGray) Color.Gray else if(isRed) ErrorRed else DeepWhite,
+                    fontSize = 24.sp,
+                )
+            }
         }
     }
 }
