a22ac628a7252b93467bb8955959858a200136b5
mjedrkowiak <mateuszjedrkowiak2@gmail.com>
2026-06-09 15:00:07 +0200

    fix: added same black for every screen :)

--- app/src/main/java/com/example/keyraceapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/keyraceapp/presentation/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.keyraceapp.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
